deb4eb344c40b6e11a28c4c62a79d7dcfa2c6471
Nikita Brizitsky <126919347+WebWave0@users.noreply.github.com>
2023-06-29 18:59:31 +0300

    Update 1.4
    Fixed absolute paths.
    Rewritten PID system.
    Removed 1 root.
    Added system error logging.

--- config.js
+++ config.js
@@ -3,11 +3,7 @@ const ini = require('ini');
 const path = require('path');
 
 const configPath = path.resolve(__dirname, 'cfg.ini');
+const configData = fs.readFileSync(configPath, 'utf-8');
+const config = ini.parse(configData);
 
-function getConfig() {
-  const configData = fs.readFileSync(configPath, 'utf-8');
-  const config = ini.parse(configData);
-  return config.Server;
-}
-
-module.exports = { getConfig };
+module.exports = config;
